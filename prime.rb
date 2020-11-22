9995c0bb9b32b1e9fc042aeaf3cd0590d391d9b2
noah <crashlive112@gmail.com>
2020-11-22 18:42:47 -0500

    commit tree c2206c4297ed8bf574abd7991629ccd5f5ad7ca8

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    elsif (2..number - 1).each do |divider|
+        if number % divider == 0
+            return false
+        end
+    end
+end
+true
+end
